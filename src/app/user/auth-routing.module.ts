263bfaa2e63609c3db7da8b5b2261642b9267b25
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-11-29 23:50:12 +0200

    Update and rename user-routing.module.ts to auth-routing.module.ts

--- src/app/user/auth-routing.module.ts
+++ src/app/user/auth-routing.module.ts
@@ -1,13 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: "user", children: 
+  { path: "auth", children: 
     [
       {
-        path: "register", component: RegisterComponent, canActivate: [AuthGuard]
+        path: "register", component: RegisterComponent
       },
       {
         path: "login", component: LoginComponent 
@@ -16,4 +15,4 @@ const routes: Routes = [
   }
 ];
 
-export const UserRoutingModule = RouterModule.forChild(routes);
+export const AuthRoutingModule = RouterModule.forChild(routes);
