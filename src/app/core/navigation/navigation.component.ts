065ebe95ea46f9ffe223708ad68602129e7f533b
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-01 23:32:21 +0200

    Update navigation.component.ts

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { IProduct } from 'src/app/shared/interfaces/product';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  
+
+  constructor(public authService: AuthService) {}
+
+  logoutHandler(): void {
+    this.authService.logout();
+  }
+
+  getCartItems(): IProduct[] {
+    return localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
+  }
+
 }
