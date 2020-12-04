c83f953c2b005307d39a5fa055a74fc1b6fa4c89
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-04 22:28:15 +0200

    add queryParams and unsubscribe on ngDestroy

--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { IProduct } from 'src/app/shared/interfaces/product';
 import { ProductService } from '../product.service';
+import { switchMap } from "rxjs/operators";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
-  id: string;
+export class ProductDetailsComponent implements OnInit, OnDestroy {
   product: IProduct;
+  subscription: Subscription;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) {
-  }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
   
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.id = params.id;
-    });
-    this.productService.getProduct(this.id).subscribe(product => {
+    this.subscription = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.productService.getProduct(params.id);
+      })
+    ).subscribe(product => {
       this.product = product;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  addToCartHandler(): void {
+    this.router.navigate(["/cart", this.product._id], { queryParams: { quantity: 1 } });
   }
 
 }
