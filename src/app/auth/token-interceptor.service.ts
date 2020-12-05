960f417c047f587a9becd02f0fdc144193de07ea
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-05 23:20:15 +0200

    add token-interceptor service

---
+++ src/app/auth/token-interceptor.service.ts
@@ -0,0 +1,18 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    let tokenizedReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${this.authService.getToken()}` }
+    });
+    return next.handle(tokenizedReq);
+  }
+}
