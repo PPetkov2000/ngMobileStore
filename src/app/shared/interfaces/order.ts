a5951a07241e31d49c72c74ec6f83685e5afdde6
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-06 02:13:55 +0200

    Update order.ts

--- src/app/shared/interfaces/order.ts
+++ src/app/shared/interfaces/order.ts
@@ -1,12 +1,14 @@
 export interface IOrder {
-  creator: string;
-  orderItems: { name: string, image: string, price: number, quantity: number, product: string}[];
-  shippingAddress: { address: string, city: string, postalCode: string, country: string};
+  _id?: string;
+  creator?: string | object;
+  orderItems: { name: string, imageUrl: string, price: number, quantity: number, product: string }[];
+  shippingAddress: { address: string, city: string, postalCode: string, country: string };
   paymentMethod: string;
+  itemsPrice: number;
   shippingPrice: number;
   taxPrice: number;
   totalPrice: number;
-  paymentResult?: { id: string, status: string, update_time: string, email_address: string};
-  isPaid: boolean;
+  paymentResult?: { id: string, status: string, update_time: string, email_address: string };
+  isPaid?: boolean;
   paidAt?: string;
 }
