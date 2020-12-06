ad662908a86989e51f644e729e897208ff9860da
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-06 02:13:08 +0200

    add register component

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { 
+  }
+  
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.authService.registerUser(form.value).subscribe(response => {
+      console.log(response);
+      localStorage.setItem("auth-token", response.token);
+      this.router.navigate(["/home"]);
+    });
+
+    form.reset();
+  }
+
+}
