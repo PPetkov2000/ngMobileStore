392ba6e1725dac4bc6321e1aa66cfc34bdabadde
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-07 23:44:38 +0200

    Update auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,40 +1,39 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { IUser } from '../shared/interfaces/user';
-
-const httpsOptions = {
-  headers: new HttpHeaders({ "Content-Type": "application/json" })
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
   baseUrl: string = "http://localhost:5000/api/users";
+  currentUser: IUser;
 
   constructor(private http: HttpClient, private router: Router) { }
 
   registerUser(data: IUser): Observable<IUser> {
-    return this.http.post<IUser>(`${this.baseUrl}/register`, data, httpsOptions);
+    return this.http.post<IUser>(`${this.baseUrl}/register`, data).pipe(tap((user) => this.currentUser = user));
   } 
 
   loginUser(data: IUser): Observable<IUser> {
-    return this.http.post<IUser>(`${this.baseUrl}/login`, data, httpsOptions);
+    return this.http.post<IUser>(`${this.baseUrl}/login`, data).pipe(tap((user) => this.currentUser = user));
   }
 
   // Helpers
 
   loggedIn(): boolean {
-    return !!localStorage.getItem("auth-token")
+    return !!localStorage.getItem("auth-token");
   }
 
   getToken() {
     return localStorage.getItem("auth-token");
   }
 
-  logout() {
+  logout(): void {
     localStorage.removeItem("auth-token")
     localStorage.removeItem("cartItems")
     localStorage.removeItem("shippingAddress")
