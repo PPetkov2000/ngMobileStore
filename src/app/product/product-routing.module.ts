70d119a769371c6dfdc92399f0ee4847086d67b7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-07 23:43:46 +0200

    Update product-routing.module.ts

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,15 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
-  {
-    path: "product", children: [
-      {
-        path: ":id", component: ProductDetailsComponent
-      },
-    ]
-  }
+  { path: "product/:id", component: ProductDetailsComponent }
 ];
 
 export const ProductRoutingModule = RouterModule.forChild(routes);
