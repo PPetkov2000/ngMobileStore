63fcd3c8501bfef4e031cb8f0aa359359c8ab342
PPetkov <ppetkov2000@abv.bg>
2020-12-08 18:33:18 +0200

    handle adding and removing from favourites

--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { IProduct } from 'src/app/shared/interfaces/product';
 import { ProductService } from '../product.service';
-import { switchMap } from "rxjs/operators";
+import { map, switchMap } from "rxjs/operators";
+import { UserService } from 'src/app/user/user.service';
+import { IProduct } from 'src/app/shared/interfaces/product';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,27 +12,80 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit, OnDestroy {
-  product: IProduct;
-  subscription: Subscription;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
+  product: IProduct;
+  productSubscription: Subscription;
+  userSubscription: Subscription;
+  message: string;
+  errorMessage: string;
+  isProductInFavourites: boolean;
+  loading: boolean;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private productService: ProductService, 
+    private userService: UserService
+  ) { }
   
   ngOnInit(): void {
-    this.subscription = this.route.params.pipe(
+    this.productSubscription = this.route.params.pipe(
       switchMap((params: Params) => {
         return this.productService.getProduct(params.id);
       })
     ).subscribe(product => {
       this.product = product;
     });
+    this.userSubscription = this.userService.getUserById("profile").pipe(
+      map((user: any) => {
+        return !!user.favouriteProducts.find((p: IProduct) => p._id === this.product._id);
+      })
+    ).subscribe((isProductInFavourites) => {
+      this.isProductInFavourites = isProductInFavourites;
+    });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.productSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   addToCartHandler(): void {
-    this.router.navigate(["/cart", this.product._id], { queryParams: { quantity: 1 } });
+    this.router.navigate(["/cart", this.product._id]);
+  }
+
+  addToFavouritesHandler(id: string): void {
+    this.loading = true;
+    this.userService.addToFavourites(id).subscribe((response) => {
+      this.message = "Product added to favourites";
+      setTimeout(() => {
+        this.message = null;
+      }, 4000);
+      this.isProductInFavourites = !this.isProductInFavourites;
+      this.loading = false;
+    }, (error) => {
+      this.errorMessage = error.error.message;
+      setTimeout(() => {
+        this.errorMessage = null;
+      }, 4000);
+    });
+  }
+
+  removeFromFavouritesHandler(id: string): void {
+    this.loading = true;
+    this.userService.removeFromFavourites(id).subscribe((response) => {
+      this.message = "Product removed from favourites";
+      setTimeout(() => {
+        this.message = null;
+      }, 4000);
+      this.isProductInFavourites = !this.isProductInFavourites;
+      this.loading = false;
+    }, (error) => {
+      this.errorMessage = error.error.message;
+      setTimeout(() => {
+        this.errorMessage = null;
+      }, 4000);
+    });
   }
 
 }
