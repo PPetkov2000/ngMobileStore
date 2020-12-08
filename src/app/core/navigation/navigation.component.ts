b116b0457d1471c605da02a7639e7e8e82894757
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-08 18:15:58 +0200

    add product search functionality

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/auth/auth.service';
+import { ProductService } from 'src/app/product/product.service';
 import { IProduct } from 'src/app/shared/interfaces/product';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService, 
+    private userService: UserService, 
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.userService.getUserById("profile").subscribe((user) => {
+      this.authService.currentUser = user;
+    });
+  }
 
   logoutHandler(): void {
     this.authService.logout();
@@ -17,6 +30,12 @@ export class NavigationComponent {
 
   getCartItems(): IProduct[] {
     return localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
+  }
+
+  searchSubmitHandler(form: NgForm): void {
+    const keyword = form.value.search;
+    this.productService.getProducts(keyword || "").subscribe();
+    form.reset();
   }
 
 }
