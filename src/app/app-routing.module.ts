acc705db263d31b59b29c0bcfe183bc19fd3d726
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-09 23:57:58 +0200

    add home pagination

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path: "", pathMatch:"full", redirectTo: "/home"
-  },
-  {
-    path: "home", component: HomeComponent
-  },
+  { path: "", pathMatch:"full", redirectTo: "/home" },
+  { path: "home", component: HomeComponent },
+  { path: "search/:keyword", component: HomeComponent },
+  { path: "home/page/:pageNumber", component: HomeComponent },
+  { path: "search/:keyword/page/:pageNumber", component: HomeComponent },
+  { path: "**", component: NotFoundComponent }
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
