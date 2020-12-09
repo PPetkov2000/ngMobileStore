861b9ffc9070a3f3813a213ac4b8dab45e377af0
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-09 23:57:33 +0200

    add pagination

---
+++ src/app/pagination/pagination/pagination.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css']
+})
+export class PaginationComponent implements OnInit {
+
+  @Input() page: number;
+  @Input() pages: number;
+  @Input() isAdmin = false;
+  @Input() adminPage = "";
+  @Input() keyword = "";
+  @Input() paginateStr = "";
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToPage(page: number): void {
+    console.log(page)
+    const url = !this.isAdmin 
+                  ? this.keyword ? `/search/${this.keyword}/page/${page}`
+                  : `${this.paginateStr}/page/${page}` 
+                : `/admin/${this.adminPage}/${page}`;
+    this.router.navigateByUrl(url);
+    this.page = page;
+  }
+
+}
