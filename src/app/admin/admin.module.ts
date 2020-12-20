3745851d5f36d37bc1b9c1c3607ebb44392d1fd9
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2020-12-20 22:41:11 +0200

    add app loader

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,6 +9,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
 import { FormsModule } from '@angular/forms';
 import { PaginationModule } from '../pagination/pagination.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { PaginationModule } from '../pagination/pagination.module';
     CommonModule,
     AdminRoutingModule,
     FormsModule,
-    PaginationModule
+    PaginationModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
